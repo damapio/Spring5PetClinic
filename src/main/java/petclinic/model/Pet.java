e95c1a14a358b675abbde562da6e35169ebbe25b
o.O <descatalogado@gmail.com>
2021-08-19 18:10:03 +0200

    3.20 POJOs iniciales. Closes #IssueNumber in GitHub

---
+++ src/main/java/petclinic/model/Pet.java
@@ -0,0 +1,33 @@
+package petclinic.model;
+
+import java.time.LocalDate;
+
+public class Pet {
+	
+	private PetType type;
+	private OwnerPet owner;
+	private LocalDate birthday;
+	
+	
+	public PetType getType() {
+		return type;
+	}
+	public void setType(PetType type) {
+		this.type = type;
+	}
+	public OwnerPet getOwner() {
+		return owner;
+	}
+	public void setOwner(OwnerPet owner) {
+		this.owner = owner;
+	}
+	public LocalDate getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(LocalDate birthday) {
+		this.birthday = birthday;
+	}
+	
+	
+
+}
---
+++ src/main/java/petclinic/model/PetType.java
@@ -0,0 +1,16 @@
+package petclinic.model;
+
+public class PetType {
+
+	private String type;
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	
+}
