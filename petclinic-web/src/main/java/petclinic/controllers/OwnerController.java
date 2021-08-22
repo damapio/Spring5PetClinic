47cdb9aa509e3aa113e04c8a18b9f6634315158a
o.O <descatalogado@gmail.com>
2021-08-22 18:09:15 +0200

    5.14 Mostrar la lista de owners

--- petclinic-web/src/main/java/petclinic/controllers/OwnerController.java
+++ petclinic-web/src/main/java/petclinic/controllers/OwnerController.java
@@ -1,14 +1,30 @@
 package petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import petclinic.service.OwnerService;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
+	
+	private final OwnerService ownerService;
+	
+	
+
+	public OwnerController(OwnerService ownerService) {
+		super();
+		this.ownerService = ownerService;
+	}
+
+
 
 	@RequestMapping({"", "/", "/index", "/index.html"})
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("owners", ownerService.findAll());
+		
 		return "owners/index";
 	}
 }
