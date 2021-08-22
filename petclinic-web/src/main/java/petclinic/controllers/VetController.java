f03b594638ffb4e8f9d3c708261e4728b68a26b7
o.O <descatalogado@gmail.com>
2021-08-22 18:14:41 +0200

    5.15 Listar los vets

--- petclinic-web/src/main/java/petclinic/controllers/VetController.java
+++ petclinic-web/src/main/java/petclinic/controllers/VetController.java
@@ -1,14 +1,25 @@
 package petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import petclinic.service.VetService;
 
 @Controller
 @RequestMapping("/vets")
 public class VetController {
+	
+	private final VetService vetService;
+
+	public VetController(VetService vetService) {
+		super();
+		this.vetService = vetService;
+	}
 
 	@RequestMapping({"", "/", "/index", "/index.html"})
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("vets", vetService.findAll());
 		return "vets/index";
 	}
 }
