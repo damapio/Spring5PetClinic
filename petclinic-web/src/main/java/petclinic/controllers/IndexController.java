0195e4bab8ea916d81b953c4b338649e95e6603b
o.O <descatalogado@gmail.com>
2021-08-23 19:31:56 +0200

    7.31 Añadida página de 404, pero con url a piñón, inútil

--- petclinic-web/src/main/java/petclinic/controllers/IndexController.java
+++ petclinic-web/src/main/java/petclinic/controllers/IndexController.java
@@ -5,9 +5,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
-
+	
 	@RequestMapping({"/", "", "index", "index.html"})
 	public String index() {
 		return "index";
+	}
+
+	@RequestMapping({"/oups", "/error"})
+	public String error() {
+		return "404";
 	}
 }
