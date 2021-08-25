cd3f2fbd878995d0efd1f8303baf69c281fc0851
o.O <descatalogado@gmail.com>
2021-08-25 20:25:34 +0200

    8.18 @MappedSuperclass para la clase base que sólo es el Id
    Le indica a Hibernate que la tenga en cuenta,
    porque vamos a heredar de ella,
    pero no le cree una tabla
    específica

--- petclinic-data/src/main/java/petclinic/model/BaseEntity.java
+++ petclinic-data/src/main/java/petclinic/model/BaseEntity.java
@@ -2,8 +2,16 @@ package petclinic.model;
 
 import java.io.Serializable;
 
-public class BaseEntity implements Serializable{
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass //para heredar, pero sin que cree una tabla para ella
+public class BaseEntity implements Serializable {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	public Long getId() {
