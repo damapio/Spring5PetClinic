8bcf7748352b467f280c4814036d448c2eb3856c
o.O <descatalogado@gmail.com>
2021-08-28 13:06:56 +0200

    10.16 Método que estaba sin implementar
    Viene bien hacer tests xD

--- petclinic-data/src/main/java/petclinic/service/map/OwnerServiceMap.java
+++ petclinic-data/src/main/java/petclinic/service/map/OwnerServiceMap.java
@@ -69,8 +69,11 @@ public class OwnerServiceMap extends AbstractMapService<OwnerPet, Long>
 
 	@Override
 	public OwnerPet findByLastName(String lastName) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.findAll()
+                .stream()
+                .filter(owner -> owner.getLastName().equalsIgnoreCase(lastName))
+                .findFirst()
+                .orElse(null);
 	}
 	
 
